ccc32db2fed903d1b4e7d4561100a50304da0956
Du Tran <trandu@gmail.com>
2016-10-11 11:09:09 -0700

    revert change on euclidean loss layer

--- src/caffe/layers/euclidean_loss_layer.cpp
+++ src/caffe/layers/euclidean_loss_layer.cpp
@@ -36,7 +36,7 @@ Dtype EuclideanLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       diff_.mutable_cpu_data());
 
   Dtype dot = caffe_cpu_dot(count, diff_.cpu_data(), diff_.cpu_data());
-  Dtype loss = dot / bottom[0]->count() / Dtype(2);
+  Dtype loss = dot / bottom[0]->num() / Dtype(2);
   if (top->size() == 1) {
      (*top)[0]->mutable_cpu_data()[0] = loss;
   }
