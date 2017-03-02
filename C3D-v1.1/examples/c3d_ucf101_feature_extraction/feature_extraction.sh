9cf4df1ffdf7d284a3a05a04482b279f1b5772d5
Du Tran <trandu@gmail.com>
2017-03-01 16:11:09 -0800

    adding feature extraction example

---
+++ C3D-v1.1/examples/c3d_ucf101_feature_extraction/feature_extraction.sh
@@ -0,0 +1,3 @@
+wget https://www.dropbox.com/s/qqfrg6h44d4jb46/c3d_resnet18_sports1m_r2_iter_2800000.caffemodel
+
+GLOG_logtostderr=1 ../../build/tools/extract_image_features.bin c3d_resnet18_ucf101_feature_extraction.prototxt c3d_resnet18_sports1m_r2_iter_2800000.caffemodel 0 30 4970 ucf101_video_frame.prefix prob pool5 res5b
--- C3D-v1.1/src/caffe/layers/black_hole_layer.cpp
+++ C3D-v1.1/src/caffe/layers/black_hole_layer.cpp
@@ -1,10 +1,18 @@
 /*
- * black_hole_layer.cpp
  *
- *  Created on: Oct 11, 2016
- *      Author: trandu
+ *  Copyright (c) 2015, Facebook, Inc. All rights reserved.
+ *
+ *  Licensed under the Creative Commons Attribution-NonCommercial 3.0
+ *  License (the "License"). You may obtain a copy of the License at
+ *  https://creativecommons.org/licenses/by-nc/3.0/.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ *
+ *
  */
-
 
 
 
