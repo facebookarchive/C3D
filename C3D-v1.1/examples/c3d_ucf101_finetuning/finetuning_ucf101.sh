f3131647cf99e0d76d24ce2a1853c0de805fbfa1
Du Tran <trandu@gmail.com>
2017-03-20 13:00:10 -0700

    adding finetuning example

---
+++ C3D-v1.1/examples/c3d_ucf101_finetuning/finetuning_ucf101.sh
@@ -0,0 +1,5 @@
+wget https://www.dropbox.com/s/qqfrg6h44d4jb46/c3d_resnet18_sports1m_r2_iter_2800000.caffemodel
+
+mkdir LOG_TRAIN
+
+GLOG_log_dir="./LOG_TRAIN" ../../build/tools/caffe.bin train --solver=solver_r2.prototxt --weights=c3d_resnet18_sports1m_r2_iter_2800000.caffemodel --gpu=0
